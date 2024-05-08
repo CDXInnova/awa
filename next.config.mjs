b04b255f255f2fd2f617c3efc08c25784b3596d6
Code X Innova S.A.C <136953870+CDXInnova@users.noreply.github.com>
2024-05-08 13:39:29 -0500

    Add files via upload

---
+++ next.config.mjs
@@ -0,0 +1,8 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+    images: {
+        domains: ['tiendasishop.com','cdn.notebookgalerie.de'],
+    },
+};
+
+export default nextConfig;
